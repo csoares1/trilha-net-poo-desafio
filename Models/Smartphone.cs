6d91dd379064f053a95c02093b0dcb7c0e72ebf4
csoares1 <carlardsouza@gmail.com>
2024-11-04 16:46:14 -0300

    resolução do desafio POO

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,11 +3,21 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+        private string Modelo{get;set;}
+        private string IMEI{get;set;}
+        private int Memoria {get;set;}
+        public Smartphone()
+        {
+            
+        }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
